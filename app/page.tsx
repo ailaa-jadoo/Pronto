22caa5b0d1e013f27ed50b5f50ecfa5b5972c92a
0oHarsho0 <harsh.chaudhary@practo.com>
2024-06-21 17:48:57 +0530

    added contact page

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,20 @@ import { Skills } from "@/components/Skills";
 import { Navbar } from "@/components/Navbar";
 import Work from "@/components/Work";
 import {Projects} from "@/components/Projects";
+import {Contact} from "@/components/Contact";
 
 export default function Home() {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <main className="dark text-foreground max-w-[1400px] mx-auto">
+        <main className="dark text-foreground max-w-[1400px] mx-auto mb-10">
           <Navbar />
           <Hero />
           <WorkEx />
           <Skills />
           <Work />
           <Projects />
+          <Contact />
         </main>
       </NextThemesProvider>
     </NextUIProvider>
